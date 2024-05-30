20ac69188b85623a16c4ea4c5f192473ccfcb3f6
shubhcctech <shubham.kurkute@cctech.co.in>
2024-05-30 15:58:01 +0530

    backend_OpenAI created

--- PerformanceAssistant/server/src/index.ts
+++ PerformanceAssistant/server/src/index.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9898;
 
-app.get('/xxx', (req: Request, res: Response) => {
+app.get('/greeting', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
   });
 
