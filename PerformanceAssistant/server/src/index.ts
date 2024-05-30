1b4b5b512309d87fbe3c413d288cdb9d0b7dee40
Suraj Soni <suraj.soni@cctech.co.in>
2024-05-30 15:43:23 +0530

    server_modified

--- PerformanceAssistant/server/src/index.ts
+++ PerformanceAssistant/server/src/index.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/xxx', (req: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
   });
 
