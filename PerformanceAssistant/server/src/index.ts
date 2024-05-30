141759a2f2d2f429a27bae1686788ba17c79046f
shubhcctech <shubham.kurkute@cctech.co.in>
2024-05-30 19:45:10 +0530

    OpenAi Connection Done

--- PerformanceAssistant/server/src/index.ts
+++ PerformanceAssistant/server/src/index.ts
@@ -1,29 +1,18 @@
 import express, { Request, Response } from 'express';
-import OpenAI from 'openai';
+import {OpenAIService} from "./OpenAIService";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const api_key = process.env.OAI_API_KEY;
+
 app.use(express.json());
-const openai = new OpenAI({
-    apiKey: api_key,
-  });
 
 app.get('/evaluvate', async (req: Request, res: Response) => {
-
-    try {
-        const completionResponse = await openai.chat.completions.create({
-            messages: [{ role: "system", content: "How are you" }],
-            model: "gpt-3.5-turbo",
-          });
-
-
-        const completion = completionResponse.choices[0].message.content;
-        res.json({ completion });
-    } catch (error) {
-        console.error('Error fetching completion from OpenAI API:', error);
-        res.status(500).json({ error: 'An error occurred while fetching the completion.' });
-    }
+    let oaiService = new OpenAIService();
+    oaiService.evaluate().then((response)=>{
+        res.send(response);
+    }).catch((error)=>{
+        res.send(error);
+    });
 });
 
 app.listen(port, () => {
