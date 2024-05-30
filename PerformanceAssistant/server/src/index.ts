b08d459816479464b6a28f376657910d89bdb308
shubhcctech <shubham.kurkute@cctech.co.in>
2024-05-30 18:04:29 +0530

    Openai

--- PerformanceAssistant/server/src/index.ts
+++ PerformanceAssistant/server/src/index.ts
@@ -1,12 +1,31 @@
 import express, { Request, Response } from 'express';
+import OpenAI from 'openai';
 
 const app = express();
-const port = process.env.PORT || 9898;
-
-app.get('/greeting', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript Express!');
+const port = process.env.PORT || 3000;
+const api_key = process.env.OAI_API_KEY;
+app.use(express.json());
+const openai = new OpenAI({
+    apiKey: api_key,
   });
 
-  app.listen(port, () => {
+app.get('/evaluvate', async (req: Request, res: Response) => {
+
+    try {
+        const completionResponse = await openai.chat.completions.create({
+            messages: [{ role: "system", content: "How are you" }],
+            model: "gpt-3.5-turbo",
+          });
+
+
+        const completion = completionResponse.choices[0].message.content;
+        res.json({ completion });
+    } catch (error) {
+        console.error('Error fetching completion from OpenAI API:', error);
+        res.status(500).json({ error: 'An error occurred while fetching the completion.' });
+    }
+});
+
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
-  });
+});
