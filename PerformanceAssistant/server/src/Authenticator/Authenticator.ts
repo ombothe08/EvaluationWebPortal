1e57b96ad1a5d5bed8ae9619a6908ad076724c85
divyaansh0105 <divyaansh.sharma@cctech.co.in>
2024-05-31 13:38:03 +0530

    Authenticator updated

--- PerformanceAssistant/server/src/Authenticator/Authenticator.ts
+++ PerformanceAssistant/server/src/Authenticator/Authenticator.ts
@@ -1,16 +1,15 @@
-// Authenticator.ts
-export interface UserCredentials {
-  UserName: string;
-  Password: string;
-}
+import { UserCredentials } from "../Interfaces/UserCredentials";
 
+
+// Authenticator.ts
 export class Authenticator {
   constructor() {
     // Initialize users array or perform any necessary setup
   }
 
   // Method to authenticate a user
-  authenticate(userCredentials: UserCredentials): boolean {
+  authenticate(userCredentials: UserCredentials): boolean 
+  {
     //  authentication logic - needs to be replaced  with  actual logic
     if (1) {
       return true;
