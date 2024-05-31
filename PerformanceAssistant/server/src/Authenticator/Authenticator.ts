f2afd62d73cbe5a106bdfa4eeca2a8ee99b5ad87
Suraj Soni <suraj.soni@cctech.co.in>
2024-05-31 17:26:07 +0530

    Authenticator file working added

---
+++ PerformanceAssistant/server/src/Authenticator/Authenticator.ts
@@ -0,0 +1,16 @@
+import { UserCredentials } from "../Interfaces/UserCredentials";
+
+
+// Authenticator.ts
+export class Authenticator {
+  private database: Database | undefined;
+
+  public async authenticate(userCredentials: UserCredentials): Promise<boolean | any> {
+
+    this.database = new Database('mongodb://localhost:27017', 'PerformanceAssistance_DB');
+    await this.database.connectToDatabase();
+
+    let result = await this.database.verifyUserCredentials(userCredentials);
+    return result;
+   }  
+}
