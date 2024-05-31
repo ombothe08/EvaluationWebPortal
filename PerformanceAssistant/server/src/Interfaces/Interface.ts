397270adbdd8029c0a329709e32c1bc47b6d0cd2
shubhcctech <shubham.kurkute@cctech.co.in>
2024-05-31 20:17:18 +0530

    DetailedAnalysis from OpenAI

--- PerformanceAssistant/server/src/Interfaces/Interface.ts
+++ PerformanceAssistant/server/src/Interfaces/Interface.ts
@@ -2,3 +2,32 @@ export interface UserCredentials {
     UserName: string;
     Password: string;
   }
+
+export interface CandidateAnalysisModel {
+    CandidateName: string;
+    Stregths: {Parameter:string, Data:string};
+    AreasOfImprovement: {Parameter:string, Data:string};
+    RecomendationForMentor: {Parameter:string, Data:string};
+  }
+
+export interface BatchAnalysisModel {
+    BatchData: {
+        BatchName:string;
+        Module: string;
+        CandidateAnalysisModel: CandidateAnalysisModel[];
+    }
+  }
+
+  export interface CandidateDataModel {
+    CandidateName: string;
+    Data: {Parameter:string, Data:string};
+  }
+
+export interface BatchDataModel {
+    BatchData: {
+        BatchName:string;
+        Module: string;
+        CandidateDataModel: CandidateDataModel[];
+    }
+  }
+  
