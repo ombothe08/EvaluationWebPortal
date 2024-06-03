569bf38548a4d8ca8947144c6579991002e6ddf9
shubhcctech <shubham.kurkute@cctech.co.in>
2024-06-03 18:12:01 +0530

    Report added in DB

--- PerformanceAssistant/server/src/OpenAIService.ts
+++ PerformanceAssistant/server/src/OpenAIService.ts
@@ -8,7 +8,7 @@ export class OpenAIService {
     const api_key = process.env.OAI_API_KEY;
         try {
             const openai = new OpenAI({apiKey: api_key});
-            const prompt = `Here is data for analysis: \n${JSON.stringify(batchData, null, 2)}
+            const prompt = `Here is data for analysis: \n${JSON.stringify(batchData)}
              Provide a detailed analysis and recommendation for mentors for the following batch data in the JSON format as specified:
              {
                 "BatchData": {
@@ -52,4 +52,53 @@ export class OpenAIService {
             return "";
         }
    }  
+  //  public async evaluateStrength(batchData:BatchDataModel): Promise<string | any> {
+
+  //   const api_key = process.env.OAI_API_KEY;
+  //       try {
+  //           const openai = new OpenAI({apiKey: api_key});
+  //           const prompt = `Here is data for analysis: \n${JSON.stringify(batchData, null, 2)}
+  //            Provide a detailed analysis and recommendation for mentors for the following batch data in the JSON format as specified:
+  //            {
+  //               "BatchData": {
+  //                 "Name": "string",
+  //                 "Module": "string",
+  //                 "AnalysisModel": [
+  //                   {
+  //                     "Name": "string",
+  //                     "Strengths": {
+  //                       "Parameter": "string",
+  //                       "Data": "string"
+  //                     },
+  //                     "AreasOfImprovement": [
+  //                       {
+  //                         "Parameter": "string",
+  //                         "Data": "string"
+  //                       }
+  //                     ],
+  //                     "InputForMentors": [
+  //                       {
+  //                         "Parameter": "string",
+  //                         "Data": "string"
+  //                       }
+  //                     ]
+  //                   }
+  //                 ]
+  //               }
+  //             }`;
+
+  //           const completionResponse = await openai.chat.completions.create({
+  //               messages: [
+  //                   { role: "system", content: "You have to compare the data of strengths from each candidate and provide strengths scale from 0 to 100." },
+  //                   { role: "user", content: prompt }
+  //               ],
+  //               model: "gpt-3.5-turbo",
+  //             });
+  //           return completionResponse.choices[0].message.content;
+
+  //       } catch (error) {
+  //           console.error('OpenAI Function exception:', error);
+  //           return "";
+  //       }
+  //  } 
 }
