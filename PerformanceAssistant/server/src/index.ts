f9a266fe88b64540e0688b8f9c1807acde3e5718
shubhcctech <shubham.kurkute@cctech.co.in>
2024-06-03 19:57:02 +0530

    Strength analyse and bar chart

--- PerformanceAssistant/server/src/index.ts
+++ PerformanceAssistant/server/src/index.ts
@@ -37,15 +37,16 @@ app.post('/evaluate', async (req: Request, res: Response) => {
   });
 });
 
-// app.post('/evaluate/strengths', async (req: Request, res: Response) => {
-//   let oaiService = new OpenAIService();
+app.post('/evaluate/strengths', async (req: Request, res: Response) => {
+  let oaiService = new OpenAIService();
   
-//   oaiService.evaluateStrength(req.body).then((response)=>{
-//       res.send(response);
-//   }).catch((error)=>{
-//       res.send(error);
-//   });
-// });
+  oaiService.evaluateStrength(req.body).then((response)=>{
+    
+      res.send(response);
+  }).catch((error)=>{
+      res.send(error);
+  });
+});
 
 
 
