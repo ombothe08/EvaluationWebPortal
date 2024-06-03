9a46568feee0a043a38a56db4d32628aaced7bc3
ombothe08 <om.bothe@cctech.co.in>
2024-06-03 13:29:42 +0530

    homepage functionality

--- PerformanceAssistant/src/components/pages/HomePage/HomePage.tsx
+++ PerformanceAssistant/src/components/pages/HomePage/HomePage.tsx
@@ -1,3 +1,186 @@
+// import React, { ChangeEvent, useState } from "react";
+// import Navbar from "../Navbar";
+// import {
+//   Box,
+//   Button,
+//   IconButton,
+//   Paper,
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableContainer,
+//   TableHead,
+//   TableRow,
+// } from "@mui/material";
+// import DeleteIcon from "@mui/icons-material/Delete";
+// import {UseExcelParametersReturn} from "../uploadFilePage/UseExcelParametersReturn";
+// import { useNavigate } from "react-router-dom";
+// import { Upload } from "@mui/icons-material";
+
+// interface HomePageProps {
+//   useExcelParameters: UseExcelParametersReturn; // Pass UseExcelParametersReturn as a prop
+// }
+
+// const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
+//   const navigate = useNavigate(); // Initialize navigate function
+//   const [file, setFile] = useState<File | null>(null);
+//   const [homepageData, setHomepageData] = useState([
+//     {
+//       analysis: "Om 1",
+//       date: "3-10-2000",
+//       operation: "",
+//     },
+//   ]);
+
+//   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+//     if (event.target.files) {
+//       useExcelParameters.handleFileUpload(event);
+//       navigate('/upload');
+//       setFile(event.target.files[0]);
+//     }
+//   };
+
+//   const handleUploadClick = () => {
+//     const fileInput = document.getElementById("fileInput") as HTMLInputElement;
+//     if (fileInput) {
+//       fileInput.click();
+//     }
+//   };
+
+//   const handleDelete = (index: number) => {
+//     const updatedData = homepageData.filter((_, i) => i !== index);
+//     setHomepageData(updatedData);
+//   };
+
+//   return (
+//     <Box
+//       sx={{
+//         minHeight: "100vh",
+//         background: "linear-gradient(to right, #38ef7d, #11998e)",
+//         py: 5,
+//       }}
+//     >
+//       <Navbar />
+
+//       <Box sx={{ display: "flex", justifyContent: "center", mt: 5 }}>
+//         <Button
+//           variant="contained"
+//           color="warning"
+//           sx={{ fontSize: "1.25rem", py: 2, px: 4 }}
+//           onClick={handleUploadClick}
+//         >
+//           Upload data to analysis
+//         </Button>
+//       </Box>
+
+//       <input
+//         type="file"
+//         id="fileInput"
+//         className="hidden"
+//         onChange={handleFileChange}
+//         style={{ display: "none" }}
+//       />
+
+//       <Box
+//         component={Paper}
+//         sx={{
+//           p: 5,
+//           borderRadius: 2,
+//           boxShadow: 3,
+//           m: 5,
+//           backgroundColor: "whitesmoke",
+//         }}
+//       >
+//         <TableContainer sx={{ maxHeight: "70vh" }}>
+//           <Table stickyHeader>
+//             <TableHead>
+//               <TableRow sx={{ backgroundColor: "papayawhip" }}>
+//                 <TableCell
+//                   sx={{
+//                     backgroundColor: "papayawhip",
+//                     fontSize: 25,
+//                     fontWeight: "bold",
+//                     border: "1px solid black",
+//                     width: "45%",
+//                   }}
+//                 >
+//                   Analysis
+//                 </TableCell>
+//                 <TableCell
+//                   sx={{
+//                     backgroundColor: "papayawhip",
+//                     fontSize: 25,
+//                     fontWeight: "bold",
+//                     border: "1px solid black",
+//                     width: "45%",
+//                   }}
+//                 >
+//                   Date
+//                 </TableCell>
+//                 <TableCell
+//                   sx={{
+//                     backgroundColor: "papayawhip",
+//                     fontSize: 25,
+//                     fontWeight: "bold",
+//                     border: "1px solid black",
+//                     width: "10%",
+//                   }}
+//                 >
+//                   Operation
+//                 </TableCell>
+//               </TableRow>
+//             </TableHead>
+//             <TableBody>
+//               {homepageData.map((candidate, index) => (
+//                 <TableRow key={index}>
+//                   <TableCell
+//                     sx={{
+//                       backgroundColor: "white",
+//                       border: "1px solid black",
+//                       padding: "8px",
+//                       width: "33.33%",
+//                     }}
+//                   >
+//                     {candidate.analysis}
+//                   </TableCell>
+//                   <TableCell
+//                     sx={{
+//                       backgroundColor: "white",
+//                       border: "1px solid black",
+//                       padding: "8px",
+//                       width: "33.33%",
+//                     }}
+//                   >
+//                     {candidate.date}
+//                   </TableCell>
+//                   <TableCell
+//                     sx={{
+//                       backgroundColor: "white",
+//                       border: "1px solid black",
+//                       padding: "8px",
+//                       width: "33.33%",
+//                     }}
+//                   >
+//                     <IconButton
+//                       color="error"
+//                       onClick={() => handleDelete(index)}
+//                     >
+//                       <DeleteIcon />
+//                     </IconButton>
+//                   </TableCell>
+//                 </TableRow>
+//               ))}
+//             </TableBody>
+//           </Table>
+//         </TableContainer>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default HomePage;
+
+
 import React, { ChangeEvent, useState } from "react";
 import Navbar from "../Navbar";
 import {
@@ -13,7 +196,8 @@ import {
   TableRow,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import {UseExcelParametersReturn} from "../uploadFilePage/UseExcelParametersReturn";
+import DownloadIcon from "@mui/icons-material/Download";
+import { UseExcelParametersReturn } from "../uploadFilePage/UseExcelParametersReturn";
 import { useNavigate } from "react-router-dom";
 import { Upload } from "@mui/icons-material";
 
@@ -50,6 +234,11 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
   const handleDelete = (index: number) => {
     const updatedData = homepageData.filter((_, i) => i !== index);
     setHomepageData(updatedData);
+  };
+
+  const handleDownload = (index: number) => {
+    // Implement the logic for downloading the data here
+    console.log(`Download clicked for index ${index}`);
   };
 
   return (
@@ -101,7 +290,7 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
                     fontSize: 25,
                     fontWeight: "bold",
                     border: "1px solid black",
-                    width: "45%",
+                    width: "30%",
                   }}
                 >
                   Analysis
@@ -112,7 +301,7 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
                     fontSize: 25,
                     fontWeight: "bold",
                     border: "1px solid black",
-                    width: "45%",
+                    width: "30%",
                   }}
                 >
                   Date
@@ -123,10 +312,21 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
                     fontSize: 25,
                     fontWeight: "bold",
                     border: "1px solid black",
-                    width: "10%",
+                    width: "20%",
                   }}
                 >
                   Operation
+                </TableCell>
+                <TableCell
+                  sx={{
+                    backgroundColor: "papayawhip",
+                    fontSize: 25,
+                    fontWeight: "bold",
+                    border: "1px solid black",
+                    width: "20%",
+                  }}
+                >
+                  Download
                 </TableCell>
               </TableRow>
             </TableHead>
@@ -138,7 +338,6 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
                       backgroundColor: "white",
                       border: "1px solid black",
                       padding: "8px",
-                      width: "33.33%",
                     }}
                   >
                     {candidate.analysis}
@@ -148,7 +347,6 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
                       backgroundColor: "white",
                       border: "1px solid black",
                       padding: "8px",
-                      width: "33.33%",
                     }}
                   >
                     {candidate.date}
@@ -158,7 +356,6 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
                       backgroundColor: "white",
                       border: "1px solid black",
                       padding: "8px",
-                      width: "33.33%",
                     }}
                   >
                     <IconButton
@@ -166,6 +363,23 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
                       onClick={() => handleDelete(index)}
                     >
                       <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      backgroundColor: "white",
+                      border: "1px solid black",
+                      padding: "8px",
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <IconButton
+                      color="primary"
+                      onClick={() => handleDownload(index)}
+                    >
+                      <DownloadIcon />
                     </IconButton>
                   </TableCell>
                 </TableRow>
@@ -179,3 +393,5 @@ const HomePage: React.FC<HomePageProps> = ({ useExcelParameters }) => {
 };
 
 export default HomePage;
+
+
