75debc4f0cc6ad4ab24779795f1c9f268c1bd67a
rashmi-kulkarni-ct402 <rashmi.kulkarni@cctech.co.in>
2024-06-05 19:37:29 +0530

    refactrored reportpge UI

--- PerformanceAssistant/src/components/pages/reportPage/ReportPage.tsx
+++ PerformanceAssistant/src/components/pages/reportPage/ReportPage.tsx
@@ -14,6 +14,9 @@ import {
   Paper,
   Button,
   IconButton,
+  alpha,
+  List,
+  ListItem,
 } from "@mui/material";
 import { ServerData } from "../../../model/evaluationData";
 import { convertDataToExcel } from "../../utils/excelUtils";
@@ -26,10 +29,8 @@ const ReportPage: React.FC = () => {
 
   const handleDetailedInsights = () => {
     navigate("/detailed-insights", { state: { data: data } });
-    
-
   };
-  const handleDownload = async (objectid: string|null) => {
+  const handleDownload = async (objectid: string | null) => {
     try {
       const response = await fetch("http://localhost:3000/getSelectedRecord", {
         method: "POST",
@@ -41,7 +42,7 @@ const ReportPage: React.FC = () => {
 
       let data: ServerData;
       data = await response.json();
-      
+
       if (response.ok) {
         convertDataToExcel(data);
       } else {
@@ -62,7 +63,8 @@ const ReportPage: React.FC = () => {
           borderRadius: 2,
           boxShadow: 3,
           m: 4,
-          backgroundColor: "aliceblue",
+          backgroundColor: alpha("#1976D2", 0.1),
+          maxWidth: "100vw",
         }}
       >
         <Typography
@@ -77,37 +79,44 @@ const ReportPage: React.FC = () => {
         >
           Team Performance Report for {data.BatchData.Name}
         </Typography>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <Typography
             variant="h5"
             component="h2"
             sx={{
               fontSize: 20,
-              fontWeight: 'bold',
+              fontWeight: "bold",
               mb: 3,
-              fontFamily: 'sans-serif',
+              fontFamily: "sans-serif",
             }}
           >
-            Module: {data.BatchData.Module || 'N/A'}
+            Module: {data.BatchData.Module || "N/A"}
           </Typography>
-          <div style={{ marginLeft: 'auto' }}>
+          <div style={{ marginLeft: "auto" }}>
+            <Button
+              color="primary"
+              variant="contained"
+              style={{ fontSize: "15px", marginRight: "20px" }}
+              onClick={handleDetailedInsights}
+            >
+              Detailed Insights
+            </Button>
             <IconButton
               color="primary"
               onClick={() => handleDownload(data.objectid)}
             >
-              <DownloadIcon style={{ fontSize: '50px' }}/>
+              <DownloadIcon style={{ fontSize: "50px" }} />
             </IconButton>
           </div>
         </div>
 
-
-        <TableContainer sx={{ maxHeight: "55vh" }}>
+        <TableContainer sx={{ maxHeight: "55vh", maxWidth: "99vw" }}>
           <Table stickyHeader>
             <TableHead>
-              <TableRow sx={{ backgroundColor: "papayawhip" }}>
+              <TableRow>
                 <TableCell
                   sx={{
-                    backgroundColor: "papayawhip",
+                    backgroundColor: alpha("#fffacd", 1),
                     fontSize: 18,
                     fontWeight: "bold",
                     border: "1px solid black",
@@ -117,7 +126,7 @@ const ReportPage: React.FC = () => {
                 </TableCell>
                 <TableCell
                   sx={{
-                    backgroundColor: "papayawhip",
+                    backgroundColor: alpha("#aff500", 1),
                     fontSize: 18,
                     fontWeight: "bold",
                     border: "1px solid black",
@@ -127,7 +136,7 @@ const ReportPage: React.FC = () => {
                 </TableCell>
                 <TableCell
                   sx={{
-                    backgroundColor: "papayawhip",
+                    backgroundColor: alpha("#ffa500", 1),
                     fontSize: 18,
                     fontWeight: "bold",
                     border: "1px solid black",
@@ -137,22 +146,23 @@ const ReportPage: React.FC = () => {
                 </TableCell>
                 <TableCell
                   sx={{
-                    backgroundColor: "papayawhip",
+                    backgroundColor: alpha("#da70d6", 1),
                     fontSize: 18,
                     fontWeight: "bold",
                     border: "1px solid black",
                   }}
                 >
-                  Input for Mentors
+                  Recommendations for Mentor
                 </TableCell>
               </TableRow>
             </TableHead>
+
             <TableBody>
               {data.BatchData.AnalysisModel.map((candidate, index) => (
                 <TableRow key={index}>
                   <TableCell
                     sx={{
-                      backgroundColor: "white",
+                      backgroundColor: alpha("#fffacd", 0.7),
                       border: "1px solid black",
                       padding: "8px",
                       fontSize: "15px",
@@ -162,63 +172,80 @@ const ReportPage: React.FC = () => {
                   </TableCell>
                   <TableCell
                     sx={{
-                      backgroundColor: "palegreen",
+                      backgroundColor: alpha("#aff500", 0.2),
                       border: "1px solid black",
                       padding: "8px",
                       fontSize: "15px",
                     }}
                   >
-                    {candidate.Strengths.map((strength, index) => (
-                      <div key={index}>
-                        {strength.Parameter}: {strength.Data}
-                      </div>
-                    ))}
+                    <List sx={{ padding: 1 }}>
+                      {candidate.Strengths.map((strength, index) => (
+                        <ListItem
+                          key={index}
+                          sx={{
+                            display: "list-item",
+                            listStyleType: "disc",
+                            padding: "2px 0",
+                            marginLeft: "15px",
+                          }}
+                        >
+                          {strength.Parameter}: {strength.Data}
+                        </ListItem>
+                      ))}
+                    </List>
                   </TableCell>
                   <TableCell
                     sx={{
-                      backgroundColor: "pink",
+                      backgroundColor: alpha("#ffa500", 0.2),
                       border: "1px solid black",
                       padding: "8px",
                       fontSize: "15px",
                     }}
                   >
-                    {candidate.AreasOfImprovement.map((improvement, index) => (
-                      <div key={index}>
-                        {improvement.Parameter}: {improvement.Data}
-                      </div>
-                    ))}
+                    <List sx={{ padding: 0 }}>
+                      {candidate.AreasOfImprovement.map(
+                        (improvement, index) => (
+                          <ListItem
+                            key={index}
+                            sx={{
+                              display: "list-item",
+                              listStyleType: "disc",
+                              padding: "2px 0",
+                              marginLeft: "15px",
+                            }}
+                          >
+                            {improvement.Parameter}: {improvement.Data}
+                          </ListItem>
+                        )
+                      )}
+                    </List>
                   </TableCell>
                   <TableCell
                     sx={{
-                      backgroundColor: "skyblue",
+                      backgroundColor: alpha("#da70d6", 0.2),
                       border: "1px solid black",
                       padding: "8px",
                       fontSize: "15px",
                     }}
                   >
-                    {candidate.InputForMentors.map((input, index) => (
-                      <div key={index}>
-                        {input.Parameter}: {input.Data}
-                      </div>
-                    ))}
+                    <List sx={{ padding: 0 }}>
+                      {candidate.InputForMentors.map((input, index) => (
+                        <ListItem
+                          key={index}
+                          sx={{
+                            display: "list-item",
+                            listStyleType: "disc",
+                            padding: "2px 0",
+                            marginLeft: "15px",
+                          }}
+                        >
+                          {input.Parameter}: {input.Data}
+                        </ListItem>
+                      ))}
+                    </List>
                   </TableCell>
                 </TableRow>
               ))}
-              <TableRow>
-              <TableCell></TableCell>
-                <TableCell>
-                  <center>
-                    <Button
-                      color="primary"
-                      variant="contained"
-                      style={{ fontSize: "15px" }}
-                      onClick={handleDetailedInsights}
-                    >
-                      Detailed Insights
-                    </Button>
-                  </center>
-                </TableCell>
-              </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
