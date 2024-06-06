cf52104d82c9d0b56eebf351b00df9dd3a0b48f6
abhicctech1003 <abhishek.more@cctech.co.in>
2024-06-06 15:05:06 +0530

    Chnage in LoginPage and LoginPageNavbar

--- PerformanceAssistant/src/components/pages/MainPage/LandingPage.tsx
+++ PerformanceAssistant/src/components/pages/MainPage/LandingPage.tsx
@@ -59,7 +59,7 @@ const LandingPage = () => {
           />
         </div>
       </div>
-      {/* {showLogin && (
+       {/* {showLogin && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <LoginPage closeLogin={() => setShowLogin(false)} />
         </div>
