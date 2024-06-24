eda0151b14e2c48d2d7a30d5ea3292890e768788
shubhcctech <shubham.kurkute@cctech.co.in>
2024-06-24 17:41:49 +0530

    OpenAi prompt changed

--- PerformanceAssistant/server/src/OpenAIService.ts
+++ PerformanceAssistant/server/src/OpenAIService.ts
@@ -10,8 +10,8 @@ export class OpenAIService {
       try {
         // Wait for the evaluate function to resolve
         let answer =  this.evaluate(candidate);
-        let json = JSON.parse(await answer);
-        let cAnalysisData = json as CandidateAnalysisModel
+        let candidateAnalysis = await answer
+        let cAnalysisData = candidateAnalysis as CandidateAnalysisModel
 
         cAnalysis.push(cAnalysisData);
       } catch (error) {
@@ -24,86 +24,106 @@ export class OpenAIService {
   }
   
 
-   public async evaluate(batchData:CandidateDataModel): Promise<string | any> {
-  
-    
-
+  public async evaluate(batchData: CandidateDataModel): Promise<string | any> {
     const api_key = process.env.OAI_API_KEY;
-        try {
-            const openai = new OpenAI({apiKey: api_key});
-            const prompt = `Here is data for analysis: \n${JSON.stringify(batchData)}
-             Provide a detailed analysis and recommendation for mentors for the following candidate data 
-             in strictly below JSON Format and dont add any characters:
-                    {
-                      "Name": "string",
-                      "Strengths": [{
-                        "Parameter": "string",
-                        "Data": "string"
-                      }],
-                      "AreasOfImprovement": [
-                        {
-                          "Parameter": "string",
-                          "Data": "string"
-                        }
-                      ],
-                      "InputForMentors": [
-                        {
-                          "Parameter": "string",
-                          "Data": "string"
-                        }
-                      ]     
-                    }`;
-
-            const completionResponse = await openai.chat.completions.create({
-                messages: [
-                    { role: "system", content: "Evaluate the candidate on the basis of data." },
-                    { role: "user", content: prompt }
-                ],
-                model: "gpt-3.5-turbo",
-              });
-            return completionResponse.choices[0].message.content;
-
-        } catch (error) {
-            console.error('OpenAI Function exception:', error);
-            return "";
-        }
-   }  
-   public async insights(strengthData:CandidateAnalysisModel[]): Promise<string | any> {
-
-    const api_key = process.env.OAI_API_KEY;
-        try {
-            const openai = new OpenAI({apiKey: api_key});
-            const prompt = `Here is data for analysis: \n${JSON.stringify(strengthData, null, 2)}
-             and compare the strengths of every candidate and provide their combine strength as well as individual strength scale from 0 to 100 in in strictly below JSON Format and dont add any characters:
-             {
-              "Data": [
-                {
-                  "Name": "string",
-                  "CombineStrength": "number",
-                  "suggestedRole": ["string"],
-                  "insight": [
-                    {
-                      "parameter": "string",
-                      "strength": "number"
-                    }
-                  ]
-                }
-              ]
+    try {
+        const openai = new OpenAI({ apiKey: api_key });
+        const prompt = `Here is data for analysis: \n${JSON.stringify(batchData)}
+        Provide a detailed analysis and recommendation for mentors for the following candidate data 
+        in strictly below JSON Format and don't add any characters:
+        \`\`\`json
+        {
+          "Name": "string",
+          "Strengths": [{
+            "Parameter": "string",
+            "Data": "string"
+          }],
+          "AreasOfImprovement": [
+            {
+              "Parameter": "string",
+              "Data": "string"
             }
-            `;
-
-            const completionResponse = await openai.chat.completions.create({
-                messages: [
-                    { role: "system", content: "You are a evaluator which evaluates and compares the strengths for each candidate on basis of different parameters individually and with combined parameters and also provide role for each candidate" },
-                    { role: "user", content: prompt }
-                ],
-                model: "gpt-3.5-turbo",
-              }); 
-            return completionResponse.choices[0].message.content;
-
-        } catch (error) {
-            console.error('OpenAI Function exception:', error);
-            return "";
+          ],
+          "InputForMentors": [
+            {
+              "Parameter": "string",
+              "Data": "string"
+            }
+          ]     
         }
-   } 
+        \`\`\``;
+
+        const completionResponse = await openai.chat.completions.create({
+            messages: [
+                { role: "system", content: "Evaluate the candidate on the basis of data." },
+                { role: "user", content: prompt }
+            ],
+            model: "gpt-3.5-turbo",
+        });
+
+        const responseContent = completionResponse.choices[0].message.content;
+       
+        // Extract JSON from the response using regex
+        const jsonMatch = responseContent!!.match(/```json([\s\S]*?)```/);
+        if (jsonMatch && jsonMatch[1]) {
+            const jsonString = jsonMatch[1].trim();
+            return JSON.parse(jsonString);
+        } else {
+            throw new Error('JSON not found in response');
+        }
+
+    } catch (error) {
+        console.error('Error evaluating candidate:', error);
+        return "";
+    }
+}
+public async insights(strengthData: CandidateAnalysisModel[]): Promise<string | any> {
+  const api_key = process.env.OAI_API_KEY;
+  try {
+      const openai = new OpenAI({ apiKey: api_key });
+      const prompt = `Here is data for analysis: \n${JSON.stringify(strengthData, null, 2)}
+      and compare the strengths of every candidate and provide their combined strength as well as individual strength scale from 0 to 100 in just the below JSON format only:
+      \`\`\`json
+      {
+        "Data": [
+          {
+            "Name": "string",
+            "CombineStrength": "number",
+            "suggestedRole": ["string"],
+            "insight": [
+              {
+                "parameter": "string",
+                "strength": "number"
+              }
+            ]
+          }
+        ]
+      }
+      \`\`\``;
+
+      const completionResponse = await openai.chat.completions.create({
+          messages: [
+              { role: "system", content: "You are an evaluator which evaluates and compares the strengths for each candidate on the basis of different parameters individually and with combined parameters and also provide role for each candidate" },
+              { role: "user", content: prompt }
+          ],
+          model: "gpt-3.5-turbo",
+      });
+
+      const responseContent = completionResponse.choices[0].message.content;
+      console.log("Response Content: ", responseContent);
+
+      // Extract JSON from the response using regex
+      const jsonMatch = responseContent!!.match(/```json([\s\S]*?)```/);
+      if (jsonMatch && jsonMatch[1]) {
+          const jsonString = jsonMatch[1].trim();
+          return JSON.parse(jsonString);
+      } else {
+          throw new Error('JSON not found in response');
+      }
+
+  } catch (error) {
+      console.error('OpenAI Function exception:', error);
+      return "";
+  }
+}
 }
